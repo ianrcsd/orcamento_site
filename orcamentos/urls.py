eecbf396489d9e4ab20355fc245210b587dd8de2
Ian Rodrigues de Castro Silva <ianrcsd@gmail.com>
2024-10-09 13:11:13 +0200

    Add Login / logged out

--- orcamentos/urls.py
+++ orcamentos/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 app_name = "orcamentos"
@@ -8,4 +10,6 @@ urlpatterns = [
     path("orcamentos/", views.lista_orcamentos, name="lista_orcamentos"),
     path("orcamentos/criar/", views.criar_orcamento, name="criar_orcamento"),
     path("orcamentos/<int:pk>/", views.detalhe_orcamento, name="detalhe_orcamento"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
